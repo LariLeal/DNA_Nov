78569dc153ce3d7edb02355e37c23a8b6aa9b7dc
LARISSA LEAL MORO <larissa.1965@aluno.pr.senac.br>
2025-11-17 09:11:04 -0300

    Criando o themes.tsx com as principais cores

---
+++ src/global/themes.tsx
@@ -0,0 +1,12 @@
+export const themas = {
+    colors: {
+        primary: '#3B82F6',
+        secondary: '#EF4444',
+        lightGray: '#d7d8d7',
+        gray: 'gray',
+        bigScreen: '#f1f7fa',
+        red: '#FF4444',
+        blueLight: '#93C5FD',
+        transparent: 'rgba(0, 0, 0, 0.5)'
+    }
+}
